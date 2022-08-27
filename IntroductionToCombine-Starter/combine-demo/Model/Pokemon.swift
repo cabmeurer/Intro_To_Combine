4dfc40c872bd0c320b8b350764d4657354e75246
Caleb Meurer <cabmeurer@gmail.com>
2022-08-27 14:11:51 -0700

    Add source files and projects

---
+++ IntroductionToCombine-Starter/combine-demo/Model/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  combine-demo
+//
+//  Created by Caleb Meurer on 8/1/22.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    var name: String
+    var url: String
+}
